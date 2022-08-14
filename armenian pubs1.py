ef0ed94c0167b7d32263c1119fd70ccaa90965f9
manchointerlink <manchog@ifact.ge>
2022-08-14 19:24:05 +0400

    exported csv

--- armenian pubs1.py
+++ armenian pubs1.py
@@ -27,6 +27,19 @@ pd.pivot_table(df,index=["Freq"],values=["Fav_Pub",],aggfunc=[np.mean,len,sum])
 # %%
 #the most favourite pub among female and male visitors
 pd.pivot_table(df,index=["Gender"],values=["Fav_Pub", "Freq"],aggfunc=[np.mean,len,sum])
+#%%
+#the most favourite pub among female and male visitors
+dfnew=pd.pivot_table(df,index=["Gender"],values=["Fav_Pub", "Freq"],aggfunc=[np.mean,len,sum])
+dfnew.to_csv('C:\\export.csv')
+df
+
+#%%
+df.to_csv('C:\\export.csv')
+df
+#%%
+df_new = pd.pivot_table(df, index=["Freq"],values=["Fav_Pub",],aggfunc=[np.mean,len,sum])
+#%%
+
 
 # %%
 df_Fav_Pub = df[(df["Fav_Pub"]== "Fav_Pub")] 
@@ -38,4 +51,16 @@ pd.pivot_table(df,index=["Age"], values=["Freq"],aggfunc=[np.mean,len,sum])
 # %%
 #What are most people willing to spend at the pub?
 pd.pivot_table(df,index=["Fav_Pub"], values=["WTS", "Freq"],aggfunc=[np.mean,len,sum])
+#%%
+df.to_csv (#the most favourite pub among female and male visitors
+pd.pivot_table(df,index=["Gender"],values=["Fav_Pub", "Freq"],aggfunc=[np.mean,len,sum]))
+# %%
+df
+# %%
+#save the data to csv file
+df.to_csv("armenian_pubs - armenian_pubs.csv")
+# %%
+df
+# %%
+df.to_csv('C:\\export.csv')
 
